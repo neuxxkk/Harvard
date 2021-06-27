76bb0e2581c11fbfcedd130674239995a2cfe045
neuxxkk <vitornms@gmail.com>
2021-06-27 03:05:09 -0300

    django, 35:30

--- files/django/lecture3/hello/urls.py
+++ files/django/lecture3/hello/urls.py
@@ -1,6 +1,10 @@
-from . import views
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("index", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("vitor", views.vitor, name="vitor"),
+    path("rafa", views.rafa, name="rafa"),
 ]
--- files/django/lecture3/hello/views.py
+++ files/django/lecture3/hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-        return HttpResponse("Hello, world!")
+        return render(request, "hello/index.html")
+
+def vitor(request):
+        return HttpResponse("Hello, vitor!")
+
+def rafa(request):
+        return HttpResponse("Hello, rafa!")
+
+def greet(request, name):
+        return render(request, "hello/greet.html", {
+                "name": name.capitalize()
+        })
