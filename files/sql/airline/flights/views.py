49c2dfe24f7115dbf244fc4298d6223e718740da
neuxxkk <vitornms@gmail.com>
2021-10-14 17:19:19 -0300

    a

--- files/sql/airline/flights/views.py
+++ files/sql/airline/flights/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import flights
+from flights.models import Flight
 
 # Create your views here.
+
+origem=Flight.origin="BH"
+destino=Flight.destination="SP"
+duracao=Flight.duration=120
+
+table = [
+    origem,
+    destino,
+    duracao
+]
+
 def index(request):
-    return render(request, "flights/index.html")
+    return render(request, "flights/index.html", {
+        "origem":origem,
+        "destino":destino,
+        "duracao":duracao,
+        "tabela":table
+
+    })
