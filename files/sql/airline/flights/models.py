f291d25b97d33b5f7119f2edb3816469dfe6ab10
neuxxkk <vitornms@gmail.com>
2021-10-15 12:16:03 -0300

    a

--- files/sql/airline/flights/models.py
+++ files/sql/airline/flights/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField, IntegerField
 
 # Create your models here.
 
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.code} ({self.city})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField() 
 
--- files/sql/airline/flights/views.py
+++ files/sql/airline/flights/views.py
@@ -3,12 +3,7 @@ from flights.models import Flight
 
 # Create your views here.
 
-f= Flight(origin="BH", destination="SP", duration=120) 
-f.save()
-
-flight0=Flight.objects.all()
 
 def index(request):
     return render(request, 'flights/index.html', {
-        "flight0":flight0
     })
